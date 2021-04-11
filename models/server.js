e46b909355786d6a849e09a545a690b1a2033e36
Andres Acero sanchez <zero@Andress-MacBook-Air.local>
2021-04-10 19:25:21 -0500

    server validations on progress

--- models/server.js
+++ models/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../dba/dbaConnection');
 
 class Server {
     constructor() {
         this.app = express();
-        this.port = process.env.PORT
+        this.port = process.env.PORT;
+        this.users_path = "/api/users";
+        this.products_path = "/api/products";
+        this.dbaConnection();
+        this.middlewares();
+        this.routes();
+    }
+    middlewares() {
+        this.app.use( cors() );
+        this.app.use( express.json() );
+    }
+    async dbaConnection() {
+        await dbConnection();
+    }
+    routes() {
+        this.app.use( this.users_path, require('../routes/user_routes') );
+        this.app.use( this.products_path, require('../routes/product_routes') );
     }
     listen() {
         this.app.listen( this.port, () => {
