5907075e579c3fe799c500051b8509a0f0c3e48e
Andres Acero sanchez <zero@Andress-MacBook-Air.local>
2021-04-11 18:33:34 -0500

    make it better

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,6 @@
+//const URL = "http://localhost:8000/";
+const URL = "https://architecturerestserver.herokuapp.com/";
 const app = document.querySelector('#app');
-axios.get('http://localhost:8000/api/products/all/').then((resp) => {
+axios.get(`${ URL }api/products/all/`).then((resp) => {
     app.innerHTML = JSON.stringify(resp.data);
 });
